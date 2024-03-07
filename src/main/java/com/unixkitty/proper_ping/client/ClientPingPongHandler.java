25011ddfaedb61af76ebab32b696db8cfdcaa12b
Unixkitty <uni@unixkitty.com>
2024-03-07 12:47:41 +0100

    Minor optimizations

--- src/main/java/com/unixkitty/proper_ping/client/ClientPingPongHandler.java
+++ src/main/java/com/unixkitty/proper_ping/client/ClientPingPongHandler.java
@@ -12,6 +12,8 @@ import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
+import java.util.stream.Collectors;
+
 @OnlyIn(Dist.CLIENT)
 @Mod.EventBusSubscriber(modid = ProperPing.MODID, value = Dist.CLIENT)
 public class ClientPingPongHandler
@@ -19,6 +21,7 @@ public class ClientPingPongHandler
     public static long pingTimeChallenge = Util.getMillis();
     public static boolean pingDataUpdated = false;
     public static int rttLatency = 0;
+    public static String queueText = "";
 
     private static int tickCount = 0;
     private static int lastPingTime = 0;
@@ -26,27 +29,31 @@ public class ClientPingPongHandler
     @SubscribeEvent
     public static void tick(final TickEvent.ClientTickEvent event)
     {
-        if (Minecraft.getInstance().getConnection() != null && !Minecraft.getInstance().isLocalServer() && event.phase == TickEvent.Phase.END)
+        if (Minecraft.getInstance().getConnection() == null || Minecraft.getInstance().isLocalServer() || event.phase != TickEvent.Phase.END)
         {
-            if (tickCount - lastPingTime >= 20)
-            {
-                lastPingTime = tickCount;
-
-                ModNetworkDispatcher.sendToServer(new PingC2SPacket(pingTimeChallenge = Util.getMillis(), rttLatency));
-            }
-
-            if (tickCount % 20 == 0 && pingDataUpdated)
-            {
-                rttLatency = calculateAverage();
-
-                pingDataUpdated = false;
-            }
-
-            tickCount = tickCount == Integer.MAX_VALUE ? 0 : ++tickCount;
+            return;
         }
+
+        if (tickCount - lastPingTime >= 20)
+        {
+            lastPingTime = tickCount;
+
+            ModNetworkDispatcher.sendToServer(new PingC2SPacket(pingTimeChallenge = Util.getMillis(), rttLatency));
+        }
+
+        if (tickCount % 20 == 0 && pingDataUpdated)
+        {
+            rttLatency = calculateAverage();
+
+            queueText = " [" + PongS2CPacket.RTT_QUEUE.stream().map(String::valueOf).collect(Collectors.joining(",")) + "]";
+
+            pingDataUpdated = false;
+        }
+
+        tickCount = tickCount == Integer.MAX_VALUE ? 0 : ++tickCount;
     }
 
-    private static int calculateAverage()
+    /*private static int calculateAverage()
     {
         if (PongS2CPacket.RTT_QUEUE.isEmpty())
         {
@@ -61,5 +68,11 @@ public class ClientPingPongHandler
         }
 
         return Math.max(sum / PongS2CPacket.RTT_QUEUE.size(), 0);
+    }*/
+
+    private static int calculateAverage()
+    {
+        return PongS2CPacket.RTT_QUEUE.isEmpty() ? 0 : Math.max(PongS2CPacket.RTT_QUEUE.stream().mapToInt(Integer::intValue).sum() / PongS2CPacket.RTT_QUEUE.size(), 0);
     }
+
 }
--- src/main/java/com/unixkitty/proper_ping/client/gui/PingOverlay.java
+++ src/main/java/com/unixkitty/proper_ping/client/gui/PingOverlay.java
@@ -2,9 +2,9 @@ package com.unixkitty.proper_ping.client.gui;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.Tesselator;
+import com.mojang.math.Matrix4f;
 import com.unixkitty.proper_ping.Config;
 import com.unixkitty.proper_ping.client.ClientPingPongHandler;
-import com.unixkitty.proper_ping.network.packet.PongS2CPacket;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiComponent;
@@ -14,8 +14,6 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.gui.overlay.ForgeGui;
 import net.minecraftforge.client.gui.overlay.IGuiOverlay;
-
-import java.util.stream.Collectors;
 
 @OnlyIn(Dist.CLIENT)
 public class PingOverlay extends GuiComponent implements IGuiOverlay
@@ -37,54 +35,64 @@ public class PingOverlay extends GuiComponent implements IGuiOverlay
     public void render(ForgeGui gui, PoseStack poseStack, float partialTick, int screenWidth, int screenHeight)
     {
         if (
-                minecraft.player != null
-                        && Config.pingHudEnabled.get()
-                        && minecraft.level != null
-                        && !minecraft.isLocalServer()
-                        && !minecraft.options.hideGui
-                        && !minecraft.options.renderDebug
+                minecraft.player == null
+                        || !Config.pingHudEnabled.get()
+                        || minecraft.level == null
+                        || minecraft.isLocalServer()
+                        || minecraft.options.hideGui
+                        || minecraft.options.renderDebug
         )
         {
-            String text = Component.translatable("multiplayer.status.ping", ClientPingPongHandler.rttLatency).getString();
-            String extraText;
-            int length;
+            return;
+        }
 
-            poseStack.pushPose();
+        String text = Component.translatable("multiplayer.status.ping", ClientPingPongHandler.rttLatency).getString();
+        String extraText;
+        int length;
 
-            MultiBufferSource.BufferSource buffer = MultiBufferSource.immediate(Tesselator.getInstance().getBuilder());
+        poseStack.pushPose();
 
-            int y = Config.verticalPadding.get() + (minecraft.font.lineHeight * Config.lineFromTop.get());
+        MultiBufferSource.BufferSource buffer = MultiBufferSource.immediate(Tesselator.getInstance().getBuilder());
 
-            if (Config.showPingQueue.get())
-            {
-                extraText = " [" + PongS2CPacket.RTT_QUEUE.stream().map(String::valueOf).collect(Collectors.joining(",")) + "]";
-                length = minecraft.font.width(text + extraText);
+        final boolean leftOrRight = Config.leftOrRight.get();
+        final int horizontalPadding = Config.horizontalPadding.get();
+        final int verticalPadding = Config.verticalPadding.get();
+        final int lineFromTop = Config.lineFromTop.get();
+        final boolean drawTextWithShadow = Config.drawTextWithShadow.get();
 
-                minecraft.font.drawInBatch(
-                        extraText,
-                        Config.leftOrRight.get() ? Config.horizontalPadding.get() + minecraft.font.width(text) : minecraft.getWindow().getGuiScaledWidth() - Config.horizontalPadding.get() - minecraft.font.width(extraText),
-                        y,
-                        WHITE,
-                        Config.drawTextWithShadow.get(),
-                        poseStack.last().pose(), buffer, false, 0, 15728880);
-            }
-            else
-            {
-                length = minecraft.font.width(text);
-            }
+        int y = verticalPadding + (minecraft.font.lineHeight * lineFromTop);
+
+        Matrix4f matrix4f = poseStack.last().pose();
+
+        if (Config.showPingQueue.get())
+        {
+            extraText = ClientPingPongHandler.queueText;
+            length = minecraft.font.width(text + extraText);
 
             minecraft.font.drawInBatch(
-                    text,
-                    Config.leftOrRight.get() ? Config.horizontalPadding.get() : minecraft.getWindow().getGuiScaledWidth() - Config.horizontalPadding.get() - length,
-                    Config.verticalPadding.get() + (minecraft.font.lineHeight * Config.lineFromTop.get()),
-                    getPingColour(ClientPingPongHandler.rttLatency),
-                    Config.drawTextWithShadow.get(),
-                    poseStack.last().pose(), buffer, false, 0, 15728880);
-
-            buffer.endBatch();
-
-            poseStack.popPose();
+                    extraText,
+                    leftOrRight ? horizontalPadding + minecraft.font.width(text) : screenWidth - horizontalPadding - minecraft.font.width(extraText),
+                    y,
+                    WHITE,
+                    drawTextWithShadow,
+                    matrix4f, buffer, false, 0, 15728880);
         }
+        else
+        {
+            length = minecraft.font.width(text);
+        }
+
+        minecraft.font.drawInBatch(
+                text,
+                leftOrRight ? horizontalPadding : screenWidth - horizontalPadding - length,
+                verticalPadding + (minecraft.font.lineHeight * lineFromTop),
+                getPingColour(ClientPingPongHandler.rttLatency),
+                drawTextWithShadow,
+                matrix4f, buffer, false, 0, 15728880);
+
+        buffer.endBatch();
+
+        poseStack.popPose();
     }
 
     public boolean renderTabListPing(PoseStack poseStack, int columnWidth, int x, int y, int latency)
